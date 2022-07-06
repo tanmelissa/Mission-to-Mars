c4a779e2b3676399f107cb36caf2636fb72b5cb0
Melissa Tan <tan.melissagrace@gmail.com>
2022-07-06 14:59:31 -0600

    added scrape_all function

--- scraping.py
+++ scraping.py
@@ -3,12 +3,27 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-
-#Set up splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
+def scrape_all():
+    
+    #initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    
+    #run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now() 
+    }
+    
+    #stop webdriver and return data
+    browser.quit()
+    return data 
 
 def mars_news(browser):
         
@@ -25,7 +40,6 @@ def mars_news(browser):
     try:
         
         slide_elem = news_soup.select_one('div.list_text')
-        slide_elem.find('div', class_='content_title')
 
         # Use the parent element to find the first `a` tag and save it as `news_title`
         news_title = slide_elem.find('div', class_='content_title').get_text()
@@ -81,9 +95,9 @@ def mars_facts():
         #convert dataframe to html table
     return df.to_html()
 
-#end the session
-browser.quit()
-
+if __name__=="__main__":
+    #if running as script, print scraped data
+    print(scrape_all())
 
 
 
